553ce63f646a3b46383fdd2ce185cb2b6b15d44d
AUTOSC <r8149762@gmail.com>
2026-02-05 02:57:05 +0700

    Add files via upload

---
+++ price/vps.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    label: "2GB RAM • 2 CPU Cores",
+    key: "r2c2",
+    price: 10000
+  },
+  {
+    label: "4GB RAM • 2 CPU Cores",
+    key: "r4c2",
+    price: 20000
+  },
+  {
+    label: "8GB RAM • 4 CPU Cores",
+    key: "r8c4",
+    price: 35000
+  },
+  {
+    label: "16GB RAM • 4 CPU Cores",
+    key: "r16c4",
+    price: 60000
+  },
+  {
+    label: "16GB RAM • 8 CPU Cores",
+    key: "r16c8",
+    price: 70000
+  },
+  {
+    label: "32GB RAM • 8 CPU Cores",
+    key: "r32c8",
+    price: 120000
+  }
+];
